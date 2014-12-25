e3fa12b3cebefc5a2d29bebf827698a2f126694d
SirWilliam92 <guisefle@gmail.com>
2014-12-25 18:27:17 +0100

    First initalization of the package

---
+++ src/direction_calculus_node.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <opencv2/core/core.hpp>
+#include <opencv/cv.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <math.h>
+
+static const double pi = 3.14159265358979323846;
+
